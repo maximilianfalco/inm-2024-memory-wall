a3742a671f1343ca27087cc2b9fed21a3ff0e234
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2024-06-07 14:52:25 +1000

    changed colors and fixed one letter input bug

--- inm-memory-wall/src/components/FloatingTextComponent.jsx
+++ inm-memory-wall/src/components/FloatingTextComponent.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box, Typography } from '@mui/material'
 import { DvdScreensaver } from 'react-dvd-screensaver'
+import { useState } from 'react';
 
 const FloatingTextComponent = (props) => {
   /**
@@ -8,13 +9,23 @@ const FloatingTextComponent = (props) => {
    * Implement latest by today (6/4/2024)
    */
 
+  const [opacity, setOpacity] = useState(0);
+  useEffect(() => {
+    const opacity = Math.random();
+    if (opacity < 0.4) {
+      setOpacity(0.4);
+    } else {
+      setOpacity(opacity);
+    }
+  }, [])
+
   return (
-    <Box sx={{ width: '100%', height: '100%', position: 'absolute', zIndex: 1 }}>
+    <Box sx={{ width: '100%', height: '100%', position: 'absolute', zIndex: 1, opacity: `${opacity}` }}>
       <DvdScreensaver freezeOnHover='true' speed={props.speed}>
         <Typography sx={{ 
           width: 'fit-content',
           color: 'black',
-          fontSize: '20px',
+          fontSize: '24px',
           fontFamily: 'FloatingText'
         }}>
           {props.text}
--- inm-memory-wall/src/components/FloatingTextContainer.jsx
+++ inm-memory-wall/src/components/FloatingTextContainer.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { Box, TextField } from '@mui/material'
+import { Box, IconButton, TextField } from '@mui/material'
 import FloatingTextComponent from './FloatingTextComponent'
 import useStateRef from 'react-usestateref'
+import ReplayRoundedIcon from '@mui/icons-material/ReplayRounded';
 
 const FloatingTextContainer = () => {
   const allowedText = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*()-_+=\\|{}[],.<>/?;:\'\"1234567890'
-  const [newText, setNewText] = useState('');
+  const [newText, setNewText, newTextRef] = useStateRef('');
   const [display, setDisplay, displayRef] = useStateRef('none');
   const updateNewText = (newText) => {
     setNewText(newText);
@@ -17,20 +18,22 @@ const FloatingTextContainer = () => {
   const [texts, setTexts] = useState([]);
   const postNewText = () => {
     const floatingTexts = texts;
-    floatingTexts.push(newText);
+    floatingTexts.push(newTextRef.current);
     setNewText('');
     setDisplay('none');
     setTexts(floatingTexts);
   }
 
-  const inputRef = useRef(null);
+  const inputRefTextInput = useRef(null);
   const handleKeyDown = (e) => {
     if (displayRef.current === 'none' && allowedText.includes(e.key)) {
       setDisplay('shown')
       updateNewText(e.key)
-      inputRef.current.focus();
     }
-    inputRef.current.focus();
+    if (newTextRef.current !== '' && e.key === 'Enter') {
+      postNewText();
+    }
+    inputRefTextInput.current.focus();
   }
 
   const [speed, setSpeed] = useState(0.04);
@@ -45,6 +48,13 @@ const FloatingTextContainer = () => {
     }
   }, []);
 
+  /**
+   * Style Constants
+   */
+  const inputStyles = {
+    style: {fontFamily: 'FloatingText', fontSize: '30px', color: '#FFFFE4'}
+  }
+
   return (
     <Box id='text-container' sx={{ width: '100%', height: '100%' }}>
       {
@@ -54,12 +64,12 @@ const FloatingTextContainer = () => {
       }
       
       <Box sx={{ 
-        backgroundColor: '#69854c',
-        // p: 1,
+        backgroundColor: '#725588',
+        bottom: '8px',
+        left: 'calc(50% - 150px)',
         position: 'fixed',
         borderRadius: '1rem',
-        width: 'fit-content',
-        maxWidth: '300px',
+        width: '300px',
         zIndex: 90,
         display: `${display}`,
       }}>
@@ -67,27 +77,27 @@ const FloatingTextContainer = () => {
           variant='outlined'
           autoComplete='off' 
           value={newText}
-          onChange={event => {
-            updateNewText(event.target.value)
-          }}
-          onKeyDown={event => {
-            if (event.key === 'Enter') {
-              postNewText();
-            } 
-          }}
-          inputRef={inputRef}
-          autoFocus
+          onChange={event => { updateNewText(event.target.value) }}
           fullWidth
+          inputRef={inputRefTextInput}
+          inputProps={inputStyles}
           sx={{
             position:'relative',
             zIndex: 2,
             "& fieldset": { border: 'none' },
           }}
-          inputProps={{
-            style: {fontFamily: 'FloatingText', fontSize: '30px'}
-          }}
+          autoFocus={true}
+          focused
           >
         </TextField>
+      </Box>
+      <Box sx={{
+        position: 'fixed',
+        zIndex: 9000000
+      }}>
+        <IconButton>
+          <ReplayRoundedIcon/>
+        </IconButton>
       </Box>
 		</Box>
   )
